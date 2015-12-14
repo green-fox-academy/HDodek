f8eaea60c662a2687f61b7e84fbc1a5585f1844c
Dorottya Hidas <dorottya.hidas@gmail.com>
2015-12-14 18:28:00 +0100

    Add Name sub menu

--- week-6/project/menu.py
+++ week-6/project/menu.py
@@ -1,13 +1,14 @@
 
 
+
 menu_items = [{"name": "New Game"},
               {"name": "Load Game"},
               {"name": "Exit"}]
 
-new_game_items = [{"name": "Re-enter name"},
-                   {"name": "Continue"},
-                   {"name": "Save"},
-                   {"name": "Quit"}]
+new_game_name_items = [{"name": "Re-enter name"},
+                       {"name": "Continue"},
+                       {"name": "Save"},
+                       {"name": "Quit"}]
 
 class Menu():
     def __init__(self, menu_items):
@@ -24,8 +25,8 @@ class Menu():
             if chooser == "" or chooser > 3:
                 raise ValueError
             elif chooser == 1:
-                new_game_menu = NewGame(new_game_items)
-                new_game_menu.menuChooser()
+                new_game_menu = NewGame(new_game_name_items)
+                new_game_menu.userName()
             elif chooser == 2:
                 pass
             elif chooser == 3:
@@ -36,15 +37,38 @@ class Menu():
     def exitGame(self):
         pass
 
-menu = Menu(menu_items)
-menu.menuChooser()
-
 class NewGame(Menu):
     def __init__(self, new_game_items):
         self.new_game_items = new_game_items
 
-    def menuChooser(self):
-        print("asvd")
+    def print_name_sub_menu(self):
+        for index, item in enumerate(self.new_game_items):
+            print(index + 1, item["name"])
 
-menu = NewGame(new_game_items)
+    def userName(self):
+        print("\033c")
+        username = input("Please, give me your name young hero! " + "\n")
+        print("God day" + " " + username + "!"+ " ")
+        self.name_sub_chooser()
+
+    def name_sub_chooser(self):
+        self.print_name_sub_menu()
+        try:
+            chooser = int(input("What\'s next?"+ "\n"))
+            if chooser == "" or chooser > 4:
+                raise ValueError
+            elif chooser == 1:
+                pass
+            elif chooser == 2:
+                pass
+            elif chooser == 3:
+                self.exit_name()
+        except:
+            print("Hey, that\'s not an option!")
+
+    def exit_name(self):
+        menuChooser()
+
+
+menu = Menu(menu_items)
 menu.menuChooser()
